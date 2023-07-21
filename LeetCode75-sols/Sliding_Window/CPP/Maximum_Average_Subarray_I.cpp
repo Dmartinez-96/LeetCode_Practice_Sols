558e1dd4f7a9c82c997e170c3a15b686efebb27d
Dmartinez-96 <85904612+Dmartinez-96@users.noreply.github.com>
2023-07-20 23:53:53 -0500

    More pracice on 7-20-23

---
+++ LeetCode75-sols/Sliding_Window/CPP/Maximum_Average_Subarray_I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int curr = 0;
+        double res = 0;
+        for (int i = 0; i < k; i++) {
+            curr += nums[i];
+            res += nums[i];
+        }
+        for (int i = k; i < nums.size(); i++) {
+            curr += nums[i] - nums[i-k];
+            if (curr >= res) {
+                res = curr;
+            }
+        }
+        return (res / k);
+    }
+};
