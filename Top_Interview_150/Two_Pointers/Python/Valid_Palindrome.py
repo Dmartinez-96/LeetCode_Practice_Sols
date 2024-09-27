879b35c1ebc2fc10f89c7d9879028501ef5fa0f5
Dakotah Martinez <85904612+Dmartinez-96@users.noreply.github.com>
2024-09-27 11:36:27 -0500

    Update Valid_Palindrome.py

--- Top_Interview_150/Two_Pointers/Python/Valid_Palindrome.py
+++ Top_Interview_150/Two_Pointers/Python/Valid_Palindrome.py
@@ -1,4 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        formatStr = ''.join([ch for ch in s.lower() if ch.isalnum()])
-        return formatStr == formatStr[::-1]
+        left, right = 0, len(s) - 1
+        while left < right:
+            # Move left pointer to next alphanumeric character
+            while left < right and not s[left].isalnum():
+                left += 1
+            # Move right pointer to next alphanumeric character
+            while left < right and not s[right].isalnum():
+                right -= 1
+            # Compare characters in lower case
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
