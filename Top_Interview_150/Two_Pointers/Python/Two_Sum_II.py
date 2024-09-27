b31efa4975714bafa1d02d13703853a1a525a3d1
Dakotah Martinez <85904612+Dmartinez-96@users.noreply.github.com>
2024-09-27 11:45:39 -0500

    Create Two_Sum_II.py

---
+++ Top_Interview_150/Two_Pointers/Python/Two_Sum_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum == target:
+                return [left + 1, right + 1]
+            elif current_sum < target:
+                # Left pointer needs to move up to higher value to increase
+                left += 1
+            else:
+                # Right pointer needs to move down to bring sum down
+                right -= 1
